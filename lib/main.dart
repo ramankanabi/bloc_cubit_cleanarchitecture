8a5dccb2ccae40b11aa41ac148d0b534a302a1f2
ramankanabi <ramankanabi@gmail.com>
2023-12-25 12:17:36 +0300

    _

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // sss
       home: BlocProvider(
         create: (context) => AdviceBloc(),
         child: const AdVicePage(),
